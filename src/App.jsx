edc51a0443ae4e20996a21afad2d3690ecef8140
JeancarlosG97 <MrJeancarlosG@Gmail.com>
2026-07-23 21:28:56 -0400

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="app">
+        <div className="login-card">
+          <h1>Car Rental System</h1>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input type="email" placeholder="Enter your email" />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+
+          <button>Login</button>
+        </div>
+      </div>
+  );
+}
+
+export default App;
+``
