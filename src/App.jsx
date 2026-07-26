b87e894424f1a2eda3bb7e65dbe7f11b252aea95
JeancarlosG97 <MrJeancarlosG@Gmail.com>
2026-07-26 19:10:42 -0400

    Connected React login to backend API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
@@ -6,42 +7,58 @@ function App() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleLogin() {
-        console.log("Login clicked");
+    async function handleLogin() {
+        try {
+            console.log("before req");
+            const response = await axios.post(
+                'https://carrental-26hx.onrender.com/auth/login',
+                {
+                    email: email,
+                    password: password
+                }
+            );
+            console.log("after req");
+            console.log(response.data);
+        } catch (error) {
+            console.log("ERROR");
 
-        console.log("Email: ", email);
-        console.log("Password: ", password);
+            if(error.respose) {
+                console.log("Status:", error.respose.status);
+                console.log("Data:", error.respose.data);
+            }
+            console.log(error);
+        }
     }
 
-  return (
-      <div className="app">
-        <div className="login-card">
-          <h1>Car Rental System</h1>
+    return (
+        <div className="app">
+            <div className="login-card">
+                <h1>Car Rental System</h1>
 
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email"
-                   placeholder="Enter email"
-                   value={email}
-                   onChange={ (e) => setEmail(e.target.value)} />
-          </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email"
+                           placeholder="Enter email"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}/>
+                </div>
 
-          <div className="form-group">
-            <label>Password</label>
-              <input
-                  type="password"
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-              />
-          </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
 
-            <button onClick={handleLogin} >
-            Login
-            </button>
+                <button onClick={handleLogin}>
+                    Login
+                </button>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
